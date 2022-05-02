49f07277c7f0f8c0f7fbb3f9c180bf281ba0e158
!nwgodrip <73049524+danunai1993@users.noreply.github.com>
2022-05-02 18:08:48 +0700

    product & data product json
    อัพเดต Product
    data products.json

--- app.js
+++ app.js
@@ -3,15 +3,27 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const produsts = require("./data/products.json");
+const productRouter = express.Router();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5555;
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")));
 
 app.set("views","./src/views");
 app.set("view engine", "ejs");
+
+productRouter.route("/").get((req,res) => {
+    res.render("products",produsts);
+})
+
+productRouter.route("/1").get((req,res) => {
+    res.send('hello world');
+})
+
+app.use("/products",productRouter);
     
 app.get('/',(req,res) =>{
     res.render('index',{username: 'danuani1993',customers:["lnwza","admin","danunai"]});
